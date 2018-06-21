46d545678a56a96db34e0eed0c2c941eca12588c
David Pay <pc_afoundo@hotmail.com>
2018-06-22 01:39:46 +0800

    many files

---
+++ test-modules/test_aiomysql.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#author: David Pay
+#date: 06-21-2018
+
+import asyncio
+from aiomysql import create_pool
+
+loop = asyncio.get_event_loop()
+
+async def go():
+    async with create_pool(host="localhost",
+    port=3306,
+    user='david',
+    password='pc56213626',
+    db='awesome',
+    loop=loop) as pool:
+
+        async with pool.get() as conn:
+            async with conn.cursor() as cur:
+                await cur.execute("select 42;")
+                value = await cur.fetchone()
+                print(value)
+
+loop.run_until_complete(go())
---
+++ www/models.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#author: David Pay
+#date: 06-21-2018
+ 
+
+import time, uuid
+
+from orm import Model, StringField, BooleanField, FloatField, TextField
+
+def next_id():
+    return '%015d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
+
+class User(Model):
+    __table__ = 'users'
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    email = StringField(ddl='varchar(50)')
+    passwd = StringField(ddl='varchar(50)')
+    admin = BooleanField()
+    name = StringField(ddl='varchar(50)')
+    image = StringField(ddl='varchar(500)')
+    created_at = FloatField(default=time.time)
+
+class Blog(Model):
+    __table__ = 'blogs'
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    user_id = StringField(ddl='varchar(50)')
+    user_name = StringField(ddl='varchar(50)')
+    user_image = StringField(ddl='varchar(500)')
+    name = StringField(ddl='varchar(50)')
+    summary = StringField(ddl='varchar(200)')
+    content = TextField()
+    created_at = FloatField(default=time.time)
+
+class Comment(Model):
+    __table__ = 'comments'
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    blog_id = StringField(ddl='varchar(50)')
+    user_id = StringField(ddl='varchar(50)')
+    user_name = StringField(ddl='varchar(50)')
+    user_image = StringField(ddl='varchar(500)')
+    content = TextField()
+    created_at = FloatField(default=time.time)
+
+
+if __name__ == "__main__":
+ 
+    import orm
+    from models import User, Blog, Comment
+    import asyncio, os, json, time
+
+    
+    def test(loop):
+        yield from orm.create_pool(loop=loop, user='david', password='pc56213626', db='awesome')
+        u = User(name='Test',email='test@example.com', passwd='w123', image='about:blank')
+        yield from u.save()
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(test(loop))
+    loop.run_forever()
+ 
+
